6d4d089cb8f8f0a72d6e8b390a397300089e6bb1
Micah Elizabeth Scott <micah@scanlime.org>
2014-01-18 18:18:17 -0500

    Change backup directory, use dash instead of underscore

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 # Quick backup to dropbox, without including the whole dataset.
 #
 
-DEST=~/Dropbox/art/2014/01jan/average_photo
+DEST=~/Dropbox/art/2014/01jan/average-photo
 
 # Account for how many files we have, but don't back them all up
 (for n in downloads failed completed; do echo $n; ls $n | wc -l; done) | tee stats.txt
