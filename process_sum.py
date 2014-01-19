ae6ec0b78ff5d1f201784343ee692fd9679c6bdd
Micah Elizabeth Scott <micah@scanlime.org>
2014-01-19 03:18:56 -0500

    More experiments in JPEG artifact removal

--- process_sum.py
+++ process_sum.py
@@ -40,6 +40,10 @@ def scaleImageCenterMinMax(s):
         numpy.min(center.reshape(-1, 3), axis=0),
         numpy.max(center.reshape(-1, 3), axis=0))
 
+def vRatio(a, b):
+    # Ratio of two vector magnitudes
+    return numpy.max(numpy.abs(a) / numpy.abs(b))
+
 def removeDctBlockArtifacts(s):
     # The raw sum buffer ends up with a very noticeable repeating 8x8 pattern due to
     # systemic errors in the JPEG compression process. We try to cancel these out by
@@ -47,15 +51,71 @@ def removeDctBlockArtifacts(s):
 
     print "Removing DCT block artifacts"
     f = numpy.fft.rfft2(s, axes=(0,1))
+    saveTiff('result-dct-spectrum-before.tiff', scaleImageChannelMinMax(numpy.log(numpy.abs(f) + 1)))
+
+    # Keep a queue of FFT hotspots to examine
+    queue = []
+
+    # Energy ratio threshold for an FFT hotspot
+    threshold = 0.5
 
     # FFT energy for random tiled 8x8 data appears in this grid pattern
-    for i in range(0, f.shape[0], s.shape[0]/8):
-        for j in range(0, f.shape[1], s.shape[1]/8):
 
-            # Zero all but DC (0,0) component
-            if i or j:
-                f[i, j] = 0
+    for i in range(0, f.shape[0]+1, s.shape[0]/8):
+        for j in range(0, f.shape[1]+1, s.shape[1]/8):
 
+            # The last row is just off the edge of the image; nudge it back in.
+            i = min(i, f.shape[0] - 1)
+            j = min(j, f.shape[1] - 1)
+
+            # Don't zero the DC component
+            if i == 0 and j == 0:
+                continue
+
+            # The energy from the DCT errors spreads outward from this point a little, in a
+            # star-like pattern. Do a flood-fill starting from this point and moving outward.
+            queue.append((i, j))
+
+    # Erode hotspots until the queue is empty, using a flood-fill algorithm.
+
+    memo = {}
+
+    while queue:
+        i, j = point = queue.pop()
+        memo[point] = True
+
+        # Zero this point
+        x = numpy.copy(f[i, j])
+        f[i, j] = 0
+
+        # Don't bother examining neigbors for points on the DC axes
+        if i == 0 or j == 0:
+            continue
+
+        # Examine neighbors
+        for i, j in ( (i-1, j),
+                      (i+1, j),
+                      (i, j-1),
+                      (i, j+1) ):
+            point = i, j
+
+            if point in memo:
+                # Already seen
+                continue
+
+            if i < 0 or i >= f.shape[0] or j < 0 or j >= f.shape[1]:
+                # Out of range
+                continue
+
+            # How different is this neighbor?
+            if vRatio(f[i, j], x) > threshold:
+                # Different enough we can stop
+                continue
+
+            # Remember to visit this neighbor
+            queue.append(point)
+
+    saveTiff('result-dct-spectrum-after.tiff', scaleImageChannelMinMax(numpy.log(numpy.abs(f) + 1)))
     return numpy.fft.irfft2(f, axes=(0,1))
 
 def gaussianBlur(s, sigma=None):
